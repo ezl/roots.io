b0ac1dbcb205700d87682ae50766599a9d192d2e
ezl <ericzliu@gmail.com>
2015-01-18 18:53:14 -0600

    Don't use containers for page structure, set max width

--- base.php
+++ base.php
@@ -12,17 +12,17 @@
     get_template_part('templates/header');
   ?>
 
-  <div class="wrap container" role="document">
-    <div class="content row">
-      <main class="main" role="main">
+  <div class="wrap" role="document">
+    <main class="main" role="main">
+      <div class="content">
         <?php include roots_template_path(); ?>
-      </main><!-- /.main -->
-      <?php if (roots_display_sidebar()) : ?>
-        <aside class="sidebar" role="complementary">
-          <?php include roots_sidebar_path(); ?>
-        </aside><!-- /.sidebar -->
-      <?php endif; ?>
-    </div><!-- /.content -->
+      </div>
+    </main><!-- /.main -->
+    <?php if (roots_display_sidebar()) : ?>
+      <aside class="sidebar" role="complementary">
+        <?php include roots_sidebar_path(); ?>
+      </aside><!-- /.sidebar -->
+    <?php endif; ?>
   </div><!-- /.wrap -->
 
   <?php get_template_part('templates/footer'); ?>
