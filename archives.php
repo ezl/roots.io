279748ae5c86add726e6b9a142eb91f5b1f15cd2
ezl <ericzliu@gmail.com>
2015-01-19 18:11:23 -0600

    Keep title padding uniform on archive.php

--- archives.php
+++ archives.php
@@ -3,37 +3,33 @@
 Template Name: Archives
 */?>
 
-<div id="container">
-    <div id="content" role="main">
-
-        <?php the_post(); ?>
-        <h1 class="entry-title"><?php the_title(); ?></h1>
-
+<!-- sorry! -->
 <style>
     table { margin-top: 25px;}
     td { padding-right: 20px; padding-bottom: 10px; vertical-align: top;}
 </style>
 
-<table id="archives">
-<?php
+<div id="container">
+    <div id="content" role="main">
+        <div class="page-header">
+            <h1 class="entry-title"><?php the_title(); ?></h1>
+        </div><!-- .page-header -->
 
-
-$args = array('posts_per_page' => 100, 'offset'=> 0, );
-
-$myposts = get_posts( $args );
-foreach ( $myposts as $post ) : setup_postdata( $post ); ?>
-    <tr>
-        <td>
-            <time class="updated"><?php the_time("M Y"); ?></time>
-        </td>
-        <td>
-            <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
-        </td>
-    </tr>
-<?php endforeach; 
-wp_reset_postdata();?>
-
-</table>
+        <table id="archives">
+        <?php
+        $args = array('posts_per_page' => 100, 'offset'=> 0, );
+        $myposts = get_posts( $args );
+        foreach ( $myposts as $post ) : setup_postdata( $post ); ?>
+            <tr>
+                <td>
+                    <time class="updated"><?php the_time("M Y"); ?></time>
+                </td>
+                <td>
+                    <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+                </td>
+            </tr>
+        <?php endforeach; wp_reset_postdata();?>
+        </table>
 
     </div><!-- #content -->
 </div><!-- #container -->
